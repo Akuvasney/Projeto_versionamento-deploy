e9ea8639a06bc6aa8d4963636c51ff1bf15413ae
User <User@DESKTOP-DKP1OM9>
2021-10-03 17:42:31 -0300

    Adicionar arquivos de projeto.

---
+++ Classe_funcionarios.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_Itau_1
+{
+    class Classe_funcionarios
+    {
+        public string nome;
+        public int cargaHoraria;
+        public double valorHora;
+
+        public Classe_funcionarios()
+        {
+
+        }
+
+        public Classe_funcionarios(string nomeVar, int cargaHorariaVar, double valorHoraVar)
+        {
+            this.nome = nomeVar;
+            this.cargaHoraria = cargaHorariaVar;
+            this.valorHora = valorHoraVar;
+        }
+        public virtual double CalculaSalario()
+        {
+            return this.valorHora*this.cargaHoraria;
+        }
+
+        public virtual void ImprimeDados()
+        {
+            Console.WriteLine("Nome: " + this.nome);
+            Console.WriteLine("Salario: " + this.CalculaSalario());
+
+        }
+    }
+
+    class Classe_terceiro : Classe_funcionarios
+    {
+        public string EmpresaOrigem { get; set; }
+        public double TaxaServico { get; set; }
+
+        public Classe_terceiro(string nome, int cargaHoraria, double valorHora, string empresaOrigem, double taxaServico)
+        {
+            this.nome = nome;
+            this.cargaHoraria = cargaHoraria;
+            this.valorHora = valorHora;
+            EmpresaOrigem = empresaOrigem;
+            TaxaServico = taxaServico;
+        }
+
+
+
+    public override double CalculaSalario()
+        {
+            return base.CalculaSalario()* this.TaxaServico;
+        }
+
+        public override void ImprimeDados()
+        {
+            base.ImprimeDados();
+            Console.WriteLine("Empresa de origem: " + this.EmpresaOrigem);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Projeto_Itau_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Classe_funcionarios funcionariaAmanda = new Classe_funcionarios();
+
+            funcionariaAmanda.nome = "Amanda";
+            funcionariaAmanda.cargaHoraria = 8;
+            funcionariaAmanda.valorHora = 30.0;
+
+            Classe_funcionarios funcionarioBruno = new Classe_terceiro("Bruno", 10, 20, "Amazon", 1.5);
+
+            Console.WriteLine("Você é terceirizado?(Y/N)");
+            string resposta = Console.ReadLine();
+
+            if (resposta == "n" || resposta == "N")
+            {
+                funcionariaAmanda.ImprimeDados();
+            }
+            else
+            {
+                funcionarioBruno.ImprimeDados();
+            }
+
+        }
+    }
+}
